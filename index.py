69fdb86691b813646c99db82264331b038d65f1f
medrees-1000 <idreesshoaib82@gmail.com>
2024-06-12 09:38:43 -0400

    First commit

---
+++ index.py
@@ -0,0 +1,34 @@
+import cv2 as cv 
+import pandas as pd
+import numpy as np 
+
+
+
+web_Cam = cv.VideoCapture(0)
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+while True:
+    ret , frame = web_Cam.read()
+    
+    if not ret:
+        break
+    
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    
+    faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+    
+    for (x, y, w, h) in faces_rect:
+        
+        cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), thickness= 2)
+        
+    cv.imshow("Face Detection", frame)
+    
+    key = cv.waitKey(1)
+     
+    
+    if key & 0xFF == ord('A'):
+        break
+    
+web_Cam.release()
+cv.destroyAllWindows()
