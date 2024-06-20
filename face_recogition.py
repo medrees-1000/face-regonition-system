d5f108a331b6a8da3fe348808afcae8aca8d7f26
medrees-1000 <idreesshoaib82@gmail.com>
2024-06-20 11:04:14 -0400

    still noting

--- face_recogition.py
+++ face_recogition.py
@@ -1,6 +1,7 @@
 import cv2
 import face_recognition
 
+
 # Load a sample picture and learn how to recognize it.
 image = face_recognition.load_image_file("Mohammad.jpg")
 known_face_encoding = face_recognition.face_encodings(image)[0]
