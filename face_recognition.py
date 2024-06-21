db5e97c0b6457b981a8e23b00292fd653068c8e3
medrees-1000 <idreesshoaib82@gmail.com>
2024-06-21 11:22:36 -0400

    Adding neccesary comments

--- face_recognition.py
+++ face_recognition.py
@@ -1,38 +1,50 @@
 import numpy as np
 import cv2 as cv
 
+# Loading haar and the name of the poeple 
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 people = ["bilal", "Mohammad"]
   
-
+# Loading the yml and face_recognizer we trained 
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('face_trained.yml')
 
-cap = cv.VideoCapture(0)  # 0 for default webcam
+# Get a reference to webcam #0 (the default one)
+cap = cv.VideoCapture(0) 
 
 while True:
+    
+    # Grab a single frame of video
     ret, frame = cap.read()
     if not ret:
         break
     
+    # Turn picture gray  
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
 
+    # Grabbing regen of interest 
     for (x, y, w, h) in faces_rect:
         faces_roi = gray[y:y+h, x:x+w]
 
+        # Predict the face and with how much confidence 
         label, confidence = face_recognizer.predict(faces_roi)
         print(f"Label = {people[label]} with a confidence of {confidence}")
 
+        # when to name  a person unknown 
         if confidence > 45:
             name = people[label]
         else:
             name = "Unknown"
 
+        # Using cv to put text on the picture to display name 
         cv.putText(frame, f"{name} {confidence:.2f}", (x, y-10), cv.FONT_HERSHEY_COMPLEX, 1.0, (0, 255, 0), 2)
         cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+    
+    # show the webcam
     cv.imshow('Detected Faces', frame)
+    
+    # how to stop the script 
     key = cv.waitKey(20)
     if key & 0xFF == ord('A'):
         break
--- face_train.py
+++ face_train.py
@@ -2,45 +2,64 @@ import os
 import cv2 as cv
 import numpy as np
 
+# Load the name of the folder that you have and the poeples name with the Path
 people = ["Mohammad", 'Bilal']
 DIR = r"C:\Downloads\people"    
 
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
+# Initialize some variables
 features = []
 labels = []
 
+
 def create_train():
+    
+    # Loop over each file 
     for person in people:
         path = os.path.join(DIR, person)
+        # join the Path to the label 
         label = people.index(person)
         
+        
+        # Go over each picture in the folder 
         for img in os.listdir(path):
+            
+            # join the path and the picture 
             img_path = os.path.join(path, img)
             
+            # Read the picture  and turn it gray 
             img_array = cv.imread(img_path)
             gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
             
+            # Face recognition
             faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor= 1.1, minNeighbors= 4)
             
+            # A loop for thr regen of interest 
             for (x,y,w,h) in faces_rect:
                 faces_roi = gray[y:y+h, x:x+w]
                 
+                # Appened both to features and labels to the variables 
                 features.append(faces_roi)
                 labels.append(label)
                 
 create_train()
 
-print("trainning done ----------------")
 
+print("trainning done ----------------")# Debugging Info
 
+# Turn the features and labels list to arrays 
 features = np.array(features , dtype='object')
 labels = np.array(labels)
+
 
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 
 face_recognizer.train(features, labels)
 
+# Make your own yml so you don't have to run the code everytime
 face_recognizer.save('face_trained.yml')
+
+# Saving them as a file using numpy.save 
 np.save('features.npy', features)
 np.save('labesl.npy', labels)
